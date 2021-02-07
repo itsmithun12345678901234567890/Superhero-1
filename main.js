fe8ee077067a255e8e2e7dca1aae48818812e91f
itsmithun12345678901234567890 <72533999+itsmithun12345678901234567890@users.noreply.github.com>
2021-02-07 12:06:29 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+var canvas = new fabric.Canvas('myCanvas');
+
+var height;
+
+var width;
+
+var player_x;
+
+var player_y;
+
+var object_of_player;
+
+var block_image="player.png";
+
+function player_update(){
+    
+}
+
+function new_image(){
+
+}
+
